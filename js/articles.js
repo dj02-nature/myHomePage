cfd80746d67811b9cf4b1f6a149619a08f9ba690
Deepak Kumar <99655458+dj02-nature@users.noreply.github.com>
2026-02-14 07:38:30 +0530

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -65,30 +65,30 @@ document.addEventListener("DOMContentLoaded", () => {
 //     : "";
 // }
 
-// function formatCoauthors(authorStr) {
-//   return " " + authorStr
-//     .replace(/\bD\. Kumar\b/g, "<strong>Checks</strong>")
-//     .replace(/;/g, ", ");
-// }
-
 function formatCoauthors(authorStr) {
-  const seen = new Set();
-
-  const authors = authorStr
-    .split(";")
-    .map(a => a.trim())
-    .filter(a => {
-      if (seen.has(a)) return false;
-      seen.add(a);
-      return true;
-    });
-
-  const formatted = authors.map(author =>
-    author === "D. Kumar" ? `<strong>${author}</strong>` : author
-  );
-
-  return authors.length ? authors.join(", ") : "";
+  return " " + authorStr
+    .replace(/\bD\. Kumar\b/g, "<strong>Checks</strong>")
+    .replace(/;/g, ", ");
 }
+
+// function formatCoauthors(authorStr) {
+//   const seen = new Set();
+
+//   const authors = authorStr
+//     .split(";")
+//     .map(a => a.trim())
+//     .filter(a => {
+//       if (seen.has(a)) return false;
+//       seen.add(a);
+//       return true;
+//     });
+
+//   const formatted = authors.map(author =>
+//     author === "D. Kumar" ? `<strong>${author}</strong>` : author
+//   );
+
+//   return authors.length ? authors.join(", ") : "";
+// }
 
 
 function formatJournalDetails(item) {
